a6a733300b37a5279c47de528ac7f4f8a28c3903
Zhou Huang <zhouh17@sjtu.edu.cn>
2019-08-15 01:30:20 +0800

    add ROOT head files required by modified testNEST.cpp

--- include/testNEST.hh
+++ include/testNEST.hh
@@ -1,3 +1,9 @@
+#ifndef TESTNEST_HH
+#define TESTNEST_HH 1
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
 using namespace std;
 using namespace NEST;
 
@@ -8,7 +14,9 @@ void GetEnergyRes(vector<double> Es);
 
 int testNEST(VDetector* detector, unsigned long int numEvts, string type,
              double eMin, double eMax, double inField, string position, string posiMuon,
-             double fPos, int seed, bool no_seed);
+             double fPos, string output_root_file, int seed, bool no_seed);
 
 double band[NUMBINS_MAX][6];
 double energies[3];
+
+#endif
